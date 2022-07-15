cee6a6bdc22946343f354fb81d7c398d897cbc3a
Rafaela G. Oliveira <87498435+ElaRafaDev@users.noreply.github.com>
2022-07-15 16:07:41 -0400

    added no.h

---
+++ arvoreBinariaDeBusca/no.h
@@ -0,0 +1,10 @@
+#ifndef ARVOREBINARIADEBUSCA_NO_H
+#define ARVOREBINARIADEBUSCA_NO_H
+
+struct no {
+    int chave;
+    struct no *esquerda;
+    struct no *direita;
+};
+
+#endif //ARVOREBINARIADEBUSCA_NO_H
